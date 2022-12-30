7e35a9f93f3e2630599471b457e0d1451cfec948
Arik Chakma <arikchangma@gmail.com>
2022-12-30 15:07:42 +0600

    chore: react query added

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,19 @@
 import '../styles/globals.css';
 import { ThemeProvider } from 'next-themes';
 import { SessionProvider } from 'next-auth/react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps, session }) {
   return (
-    <SessionProvider session={session}>
-      <ThemeProvider attribute="class">
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </SessionProvider>
+    <QueryClientProvider client={queryClient}>
+      <SessionProvider session={session}>
+        <ThemeProvider attribute="class">
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </SessionProvider>
+    </QueryClientProvider>
   );
 }
 
