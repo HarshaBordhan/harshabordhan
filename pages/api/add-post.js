2c94d5edc2c67e9d08b5be236a64d8381bde4b11
Arik Chakma <arikchangma@gmail.com>
2022-12-30 15:05:10 +0600

    chore: add post api endpoint

--- pages/api/add-post.js
+++ pages/api/add-post.js
@@ -1,12 +1,34 @@
 import prisma from '../../lib/prisma';
+import { unstable_getServerSession } from 'next-auth';
+import authOptions from './auth/[...nextauth]';
 
 export default async function handler(req, res) {
-  const { id } = req.query;
-  const message = await prisma.messages.create({
+  if (req.method !== 'POST') {
+    res.setHeader('Allow', ['POST']);
+    return res.status(405).json({ error: 'Method not allowed' });
+  }
+
+  const session = await unstable_getServerSession(req, res, authOptions);
+  if (!session) {
+    return res.status(401).json({ error: 'Not authenticated' });
+  }
+
+  const { message } = req.body;
+
+  if (!message) {
+    return res.status(400).json({ error: 'Message is required' });
+  }
+
+  const createdMessage = await prisma.messages.create({
     data: {
-      msg: 'Hello Arikko',
-      userId: id,
+      msg: message,
+      user: {
+        connect: {
+          id: session.user.id,
+        },
+      },
     },
   });
-  res.status(200).json({ message });
+
+  res.status(200).json({ createdMessage });
 }
