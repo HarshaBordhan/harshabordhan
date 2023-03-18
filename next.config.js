9e923585afaee7a848b30deb00582b4575863f2c
Harsha Bordhan Chakma <100533487+HarshaBordhan@users.noreply.github.com>
2023-03-19 03:57:16 +0600

    Changed Styles

--- next.config.js
+++ next.config.js
@@ -2,22 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  module: {
-    // …
-    rules: [
-      // …
-      {
-        test: /\.mdx?$/,
-        use: [
-          {
-            loader: '@mdx-js/loader',
-            /** @type {import('@mdx-js/loader').Options} */
-            options: {},
-          },
-        ],
-      },
-    ],
-  },
 };
 
 module.exports = nextConfig;
