377bb9448d149f07253151be3b2f43d20d4b8d41
HarshaBordhan <harshabordhanc@gmail.com>
2025-09-18 03:26:05 +0600

    feat: rebuilding the ui - nav and hero

---
+++ src/components/Hero.tsx
@@ -0,0 +1,68 @@
+import { ArrowDown, Mail, Linkedin, Github } from "lucide-react";
+import { Button } from "./reusable/button";
+import Link from "next/link";
+
+export default function Hero() {
+  return (
+    <section className="min-h-screen flex justify-center items-center">
+      <div className="mx-auto max-w-7xl px-4 sm:px-10">
+        <div className="flex flex-col gap-6">
+          <div className="h-full flex items-center gap-20">
+            <div className="w-1/2 flex flex-col gap-8">
+              <div className="space-y-6">
+                <h2 className="text-7xl font-medium font-dm-sans whitespace-pre-wrap wrap-break-word">
+                  HarshaBordhan Development
+                </h2>
+                <p className="max-w-[500px] text-lg font-medium font-dm-sans whitespace-pre-wrap break-words">
+                  Building dynamic, efficient and effective frontend
+                  experiences, that fit right into your existing processes
+                </p>
+              </div>
+
+              <div className="flex gap-4">
+                <Button
+                  variant="ghost"
+                  size="icon"
+                  className="hover:text-primary"
+                >
+                  <Github className="w-5 h-5" />
+                </Button>
+                <Button
+                  variant="ghost"
+                  size="icon"
+                  className="hover:text-primary"
+                >
+                  <Linkedin className="w-5 h-5" />
+                </Button>
+                <Button
+                  variant="ghost"
+                  size="icon"
+                  className="hover:text-primary"
+                >
+                  <Mail className="w-5 h-5" />
+                </Button>
+              </div>
+            </div>
+
+            <div className="w-1/2 self-end flex flex-col items-end gap-1 font-dm-sans relative">
+              <Link href="" className="font-medium">
+                harsha3dev@gmail.com
+              </Link>
+
+              <span className="text-muted-foreground">Scroll to explore</span>
+              <div className="absolute -bottom-[108%]">
+                <Button
+                  className="w-10 h-10 rounded-full border border-border cursor-pointer hover:bg-muted transition-all duration-300 animate-bounce"
+                  variant="ghost"
+                  size="icon"
+                >
+                  <ArrowDown size={5} />
+                </Button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import Container from "@/components/container/Container";
+import Link from "next/link";
+import { FramerButton } from "@/components/buttons/FramerButton";
+import { useEffect, useState } from "react";
+import { Button } from "./reusable/button";
+import { Download } from "lucide-react";
+
+export default function Navbar() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  const navItems = [
+    { label: "Work", href: "#projects" },
+    { label: "About", href: "#about" },
+    { label: "Contact", href: "#contact" },
+    // { label: "Blog", href: "#blog" },
+  ];
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 50);
+    };
+
+    window.addEventListener("scroll", () => handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <nav
+      className={`fixed top-0 left-0 right-0 z-50 transition-all duration-300"
+        ${isScrolled ? "bg-background/80 backdrop-blur-md" : "bg-transparent"}
+      `}
+    >
+      <Container>
+        <div>
+          <div className="flex justify-between items-center py-7">
+            <div className="text-lg cursor-pointer hover:text-muted-foreground duration-200">
+              HarshaDev
+            </div>
+            <div className="flex items-center gap-6">
+              {navItems.map((item) => (
+                <Link href={item.href} key={item.href}>
+                  <FramerButton className="cursor-pointer font-dm-sans">
+                    {item.label}
+                  </FramerButton>
+                </Link>
+              ))}
+              <Button
+                variant="outline"
+                size="lg"
+                className="self-start gap-2 cursor-pointer"
+              >
+                <Download size={5} />
+                Download CV
+              </Button>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </nav>
+  );
+}
