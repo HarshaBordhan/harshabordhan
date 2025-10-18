28e4c93f08f77f1b75702d28d2df27af2da9a081
HarshaBordhan <harshabordhanc@gmail.com>
2025-10-18 14:48:14 +0600

    feat:added footer file

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import Container from "./container/Container";
+
+export default function Footer() {
+  return (
+    <footer className="">
+      <Container>
+        <div></div>
+      </Container>
+    </footer>
+  );
+}
